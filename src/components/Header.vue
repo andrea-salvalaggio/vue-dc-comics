eb90d684c265698a059c890c6fbc452b0631195e
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-15 15:51:32 +0200

    Header stilizzato

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="d-flex justify-content-around align-items-center mt-4">
+          <img src="../assets/img/dc-logo.png" alt="DC logo">
+          <ul>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Collectibles</li>
+            <li>Videos</li>
+            <li>Fans</li>
+            <li>News</li>
+            <li>Shop</li>
+          </ul>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    header{
+        background-color: #ffff;
+    }
+
+    ul{
+        list-style: none;
+    }
+
+    li{
+        display: inline-block;
+        padding: 0 10px;
+    }
+    
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    main{
+        background-color: #1c1c1c;
+    }
+    
+</style>
