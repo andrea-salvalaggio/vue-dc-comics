4e0dbbdef235f12de0f4b3f69acbd9cbfb3a7667
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-07-18 16:26:55 +0200

    inserito le card tramite props

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="img-container">
+            <img :src="imgUrl" :alt="title">
+        </div>
+        <h6 class="mb-0"> {{ title }} </h6>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        'imgUrl': String,
+        'title': String,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .img-container{
+        width: 200px;
+        height: 200px;
+        margin: 0 auto;
+
+        img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: top;
+        }
+    }
+
+    h6{
+        color: white;
+        text-transform: uppercase;
+        font-weight: 400;
+        margin-top: 10px;
+    }
+
+</style>
